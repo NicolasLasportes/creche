b59e3cbab4f7c76a82adf6d21647de7f5fa7fdb3
nicolas <nicolas.lasportes@gmail.com>
2018-10-31 12:12:24 +0100

    dernier commit

---
+++ activite.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Activités</title>
+	<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<h1>Liste des activités</h1>
+
+</body>
+</html>
---
+++ admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Administration</title>
+		<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	</head>
+	<body>
+		<h1>Page d'administration</h1>
+
+	</body>
+</html>
